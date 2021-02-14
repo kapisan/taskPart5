dee5219608a42b4420c31713d59e882b2a894e52
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-02-14 14:26:06 +0900

    フィードバックを修正

--- TaskPart5/ViewController.swift
+++ TaskPart5/ViewController.swift
@@ -13,34 +13,36 @@ class ViewController: UIViewController {
     @IBOutlet private weak var label: UILabel!
 
     @IBAction private func button(_ sender: UIButton) {
-        let textFieldNum1 = Double(textField1.text!)
-        let textFieldNum2 = Double(textField2.text!)
-        let title = "課題5"
-        // OKボタンを作成&追加
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-
-        if textFieldNum1 == nil {
-            // アラートを作成
-            let alert = UIAlertController(title: title, message: "割られる数を入力してください", preferredStyle: .alert)
-
-            present(alert, animated: true, completion: nil)
-            alert.addAction(okAction)
-        } else if textFieldNum2 == nil {
-            // アラートを作成
-            let alert = UIAlertController(title: title, message: "割る数を入力してください", preferredStyle: .alert)
-
-            present(alert, animated: true, completion: nil)
-            alert.addAction(okAction)
-        } else if textFieldNum2 == 0 {
-            // アラートを作成
-            let alert = UIAlertController(title: title, message: "割る数には、0を入力しないでください", preferredStyle: .alert)
-
-            present(alert, animated: true, completion: nil)
-            alert.addAction(okAction)
-        } else {
-            let result = textFieldNum1! / textFieldNum2!
-            let resultRound = round(result*100000)/100000
-            label.text = String(resultRound)
+        guard let textFieldNum1 = Double(textField1.text!) else {
+            presentAlert(message: "割られる数を入力してください")
+            return
         }
+
+        guard let textFieldNum2 = Double(textField2.text!) else {
+            presentAlert(message: "割る数を入力してください")
+            return
+        }
+
+        guard textFieldNum2 != 0 else {
+            presentAlert(message: "割る数には、0を入力しないでください")
+            return
+        }
+
+        let result = textFieldNum1 / textFieldNum2
+
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 5
+
+        label.text = formatter.string(from: NSNumber(value: result))
+    }
+
+    private func presentAlert(message: String) {
+        let title = "課題5"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        present(alert, animated: true, completion: nil)
+
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
     }
 }
